530a37e6dec02b1521e84ea04127cd13cd4c1813
Brian Ortiz <mateo972000@gmail.com>
2023-05-26 13:01:14 -0400

    Initial Commit

---
+++ SeeFoodCoreMLSwiftUI/SeeFoodCoreMLSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeeFoodCoreMLSwiftUIApp.swift
+//  SeeFoodCoreMLSwiftUI
+//
+//  Created by Brian Ortiz on 2023-05-26.
+//
+
+import SwiftUI
+
+@main
+struct SeeFoodCoreMLSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
